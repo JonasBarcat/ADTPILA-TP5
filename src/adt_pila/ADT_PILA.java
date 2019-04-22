8391488f59496beef38629bd3f373b7b60eb060a
Jony <jonas_barcat.06@hotmail.com>
2019-04-22 19:51:55 -0300

    ADT Pila Listo. Operaciones: EspilaVacia,Push,Top,Pop,PilaVacia(consctructor pila)

--- src/adt_pila/ADT_PILA.java
+++ src/adt_pila/ADT_PILA.java
@@ -16,6 +16,39 @@ public class ADT_PILA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Item algo1=new Item('a');
+        Item algo2=new Item('b');
+        
+        //creacion de una pila vacia
+        Pila p1=new Pila();
+       
+        
+        p1.Push(algo1);
+        p1.Push(algo2);
+        
+         if(p1.EsPilaVacia()){
+            System.out.println("La pila se encuentra vacía");
+        }else{
+            System.out.println("La pila no se encuentra vacía");
+        }
+        
+        p1.verPila();
+        
+        System.out.println("El elemento ubicado en el tope de la pila es: "+p1.Top().getPropiedad());
+        
+        System.out.println("Se produjo la eliminacion del elemento tope de la lista (POP): ");
+        p1.Pop();
+        p1.verPila();
+        
+        
+        Pila p2=new Pila();
+        
+        p2.verPila();
+        p2.Top();
+        p2.Pop();
+        
+        
     }
     
 }
--- src/adt_pila/Pila.java
+++ src/adt_pila/Pila.java
@@ -17,9 +17,54 @@ public class Pila {
     }
     
  
+    public boolean EsPilaVacia(){ //será instanciado por un objeto del tipo pila
+        Item aux=this.primero;
+            if(aux==null){
+                return true;
+            }else return false;
+    }
     
     
+    public void Push(Item algo){ // recibe algo del tipo item y lo agrega a la pila que instancia el metodo
+        algo.setSiguiente(this.primero);
+        this.primero=algo;
+    }
     
+    public Item Top(){// devuelve el ultimo elemento en el tope de la pila actual
+           if(this.primero==null){
+              System.out.println("Lista vacía, no es posible devolver ningun elemento");
+              return null; // null representaria el item indefinido
+           }else{
+              Item aux=this.primero;
+              return aux; // por las dudas trabajo con un auxiliar}  
+           }
+    }
+    
+    
+    public void Pop(){// retorna la la lista menos el elemento del tope
+         if(this.primero==null){
+              System.out.println("Lista vacía, no es posible eliminar ningun elemento");
+           }else{
+              this.primero=this.primero.getSiguiente(); // seteo el puntero primero en el 2do item
+           }
+    }
+    
+    
+   
+    
+    public void verPila(){  //ver los elementos cargados en la pila que instancia el metodo
+            if(this.primero==null){
+             System.out.println("Lista vacía");
+            }else{
+              Item aux=this.primero;
+              System.out.println("Items cargados en la pila: ");
+                while(aux!=null){
+                    System.out.println("    Item: "+aux.getPropiedad());
+                    aux=aux.getSiguiente();
+                 }
+            }
+            
+    }
     
     
 }
